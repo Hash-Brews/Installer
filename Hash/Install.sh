5a724d5d999344f5b31625ecf5fa99f954fe0933
PuppyStudios1 <148756635+PuppyStudios1@users.noreply.github.com>
2023-12-18 10:54:02 +1100

    Add files via upload

---
+++ Hash/Install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the HashBrew repository
+git clone https://github.com/Hash-Brews/HashBrew.git
+
+# Move to the cloned directory
+cd HashBrew 1.4.0
+
+# Print confirmation message
+echo "HashBrew Installed successfully!"
