127098ac7c902a292b5a5fc3502ec2badc0eff16
PuppyStudios1 <148756635+PuppyStudios1@users.noreply.github.com>
2024-01-03 21:23:55 +1100

    Update Install.sh

--- Hash/Install.sh
+++ Hash/Install.sh
@@ -3,6 +3,18 @@
 # Clone the HashBrew repository
 git clone https://github.com/Hash-Brews/HashBrew.git
 
+# download python3
+sudo apt install python3
+
+# install pip
+sudo apt install pip3
+
+# install all packages
+sudo pip3 install time
+sudo pip3 install os
+sudo pip3 install github
+sudo pip3 install git
+
 # Move to the cloned directory
 cd HashBrew 1.4.0
 
